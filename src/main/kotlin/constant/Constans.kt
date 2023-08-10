eb3b384d8e8c5539380ab9b5a07785ccbeafb85f
alidehkhodaei <alidehkhodaei1998@gmail.com>
2023-08-10 14:39:58 +0330

    Add Constants file

---
+++ src/main/kotlin/constant/Constans.kt
@@ -0,0 +1,31 @@
+package constant
+
+/**
+ * Constants
+ *
+ * This package contains various constants used throughout the application.
+ **/
+const val ACCEPT_HEADER = "accept"
+const val JSON_CONTENT_TYPE = "application/json"
+const val CONTENT_TYPE = "Content-Type"
+const val GITTER_API_BASE_URL = "https://api.gitterapp.com/"
+const val TELEGRAM_API_BASE_URL = "https://api.telegram.org/"
+const val CHANNEL_ID_USED_IN_MESSAGE = "@github\\_trending\\_channel"
+const val CHANNEL_ID_USED_IN_REQUEST_BODY = "@github_trending_channel"
+const val MAX_SIZE_MESSAGE = 4096
+const val LIMIT_SIZE_DESCRIPTION = 100
+const val DOES_NOT_EXIST_MESSAGE = "DOES NOT EXIST"
+const val IMAGE_WIDTH = 1000
+const val IMAGE_HEIGHT = 600
+const val MARKDOWN = "markdown"
+const val GET_REQUEST = "GET"
+const val POST_REQUEST = "POST"
+const val HTTP_OK = 200
+const val CHAT_ID="chat_id"
+const val PHOTO="photo"
+const val CAPTION="caption"
+const val PARSE_MODE="parse_mode"
+const val TEXT="text"
+const val DISABLE_WEB_PAGE_PREVIEW="disable_web_page_preview"
+const val SEND_MESSAGE_URL="sendMessage"
+const val SEND_PHOTO_URL="sendPhoto"
